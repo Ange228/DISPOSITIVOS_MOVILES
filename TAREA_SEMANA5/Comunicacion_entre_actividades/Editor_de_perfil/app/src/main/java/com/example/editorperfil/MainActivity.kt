d5c281d14b62fd935d0afc45ce11c4113c83b4e5
angelicavaleria <angelica.ctovar22@gmail.com>
2025-10-01 15:24:36 -0500

    Agregando edit text, text view y culminando con la edicion del activity main

--- TAREA_SEMANA5/Comunicacion_entre_actividades/Editor_de_perfil/app/src/main/java/com/example/editorperfil/MainActivity.kt
+++ TAREA_SEMANA5/Comunicacion_entre_actividades/Editor_de_perfil/app/src/main/java/com/example/editorperfil/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.editorperfil
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txtName = findViewById<EditText>(R.id.edtName)
+        val txtEdad = findViewById<EditText>(R.id.edtEdad)
+        val txtCiudad = findViewById<EditText>(R.id.edtCiudad)
+        val txtCorreo= findViewById<EditText>(R.id.edtCorreo)
+        val txtName = findViewById<EditText>(R.id.edtName)
 
     }
 }
