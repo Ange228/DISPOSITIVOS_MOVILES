d924b3ce416fdfa2fc1d8cf228c4cd7ef9ec31c8
angelicavaleria <angelica.ctovar22@gmail.com>
2025-10-01 15:53:34 -0500

    declarando variables para la optencion del id

--- TAREA_SEMANA5/Comunicacion_entre_actividades/Editor_de_perfil/app/src/main/java/com/example/editorperfil/MainActivity.kt
+++ TAREA_SEMANA5/Comunicacion_entre_actividades/Editor_de_perfil/app/src/main/java/com/example/editorperfil/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.editorperfil
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -10,11 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val txtName = findViewById<EditText>(R.id.edtName)
-        val txtEdad = findViewById<EditText>(R.id.edtEdad)
-        val txtCiudad = findViewById<EditText>(R.id.edtCiudad)
-        val txtCorreo= findViewById<EditText>(R.id.edtCorreo)
-        val txtName = findViewById<EditText>(R.id.edtName)
+        val editTextName = findViewById<EditText>(R.id.edtName)
+        val editTextEdad= findViewById<EditText>(R.id.edtEdad)
+        val editTextCiudad= findViewById<EditText>(R.id.edtCiudad)
+        val editTextCorreo= findViewById<EditText>(R.id.edtCorreo)
+        val btnEnviar = findViewById<Button>(R.id.btnEnviar)
 
+        btnEnviar.setOnClickListener{
+            val nombre = editTextName.text.toString()
+            val edad = editTextEdad.text.toString().toInt()
+            val ciudad = editTextCiudad.text.toString()
+            val correo = editTextCorreo.text.toString()
+        }
     }
 }
